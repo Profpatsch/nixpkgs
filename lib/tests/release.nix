97233eef2590e398cfafffce5d97260bfa44df09
Profpatsch <mail@profpatsch.de>
2017-03-19 00:51:24 +0100

    lib/tests: extract local nix setup code
    The setup of environment variables can be used by other tests to set up a local
    environment for nix invocations.
    Preparation for use in package-tests.

--- lib/tests/release.nix
+++ lib/tests/release.nix
@@ -6,22 +6,8 @@ with lib;
 stdenv.mkDerivation {
   name = "nixpkgs-lib-tests";
   buildInputs = [ nix ];
-  NIX_PATH="nixpkgs=${nixpkgs}";
-
   buildCommand = ''
-    datadir="${nix}/share"
-    export TEST_ROOT=$(pwd)/test-tmp
-    export NIX_BUILD_HOOK=
-    export NIX_CONF_DIR=$TEST_ROOT/etc
-    export NIX_DB_DIR=$TEST_ROOT/db
-    export NIX_LOCALSTATE_DIR=$TEST_ROOT/var
-    export NIX_LOG_DIR=$TEST_ROOT/var/log/nix
-    export NIX_MANIFESTS_DIR=$TEST_ROOT/var/nix/manifests
-    export NIX_STATE_DIR=$TEST_ROOT/var/nix
-    export NIX_STORE_DIR=$TEST_ROOT/store
-    export PAGER=cat
-    cacheDir=$TEST_ROOT/binary-cache
-    nix-store --init
+    source ${import ./local-nix-store.nix { inherit nixpkgs nix writeText; }}
 
     cd ${nixpkgs}/lib/tests
     ./modules.sh
