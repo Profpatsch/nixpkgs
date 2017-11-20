80729e46db9db08afbd58622b94a1a0ce372c3b1
Profpatsch <mail@profpatsch.de>
2017-11-20 12:21:40 +0100

    nixos/modules/testing/tests: add options.tests
    An option for keeping module tests close to the modules they belong to.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -725,6 +725,7 @@
   ./tasks/trackpoint.nix
   ./tasks/powertop.nix
   ./testing/service-runner.nix
+  ./testing/tests.nix
   ./virtualisation/container-config.nix
   ./virtualisation/containers.nix
   ./virtualisation/docker.nix
