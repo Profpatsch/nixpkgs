c9989a7287484f4a61f81779fdd36e5f1a19cb48
Profpatsch <mail@profpatsch.de>
2018-07-22 21:44:10 +0200

    skarnet software: add Profpatsch as maintainer

--- pkgs/os-specific/linux/s6-linux-utils/default.nix
+++ pkgs/os-specific/linux/s6-linux-utils/default.nix
@@ -29,7 +29,7 @@ in stdenv.mkDerivation rec {
     description = "A set of minimalistic Linux-specific system utilities";
     platforms = stdenv.lib.platforms.linux;
     license = stdenv.lib.licenses.isc;
-    maintainers = with stdenv.lib.maintainers; [ pmahoney ];
+    maintainers = with stdenv.lib.maintainers; [ pmahoney Profpatsch ];
   };
 
 }
