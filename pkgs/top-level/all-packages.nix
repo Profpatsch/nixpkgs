45882db66062a4d5b049f2dc9ea57756a4e7f9f9
Profpatsch <mail@profpatsch.de>
2022-10-18 15:45:10 +0200

    decode-spam-headers: init at 2022-09-22-unreleased

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5693,6 +5693,8 @@ with pkgs;
 
   debugedit = callPackage ../development/tools/misc/debugedit { };
 
+  decode-spam-headers = callPackage ../tools/networking/decode-spam-headers { };
+
   deer = callPackage ../shells/zsh/zsh-deer { };
 
   delta = callPackage ../applications/version-management/git-and-tools/delta {
